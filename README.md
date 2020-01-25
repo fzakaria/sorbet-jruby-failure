ee5ac4beea667a61910fa2ca4e8fd4da1e2c5cb2
Farid Zakaria <farid.m.zakaria@gmail.com>
2020-01-25 13:06:00 -0800

    updated example

--- README.md
+++ README.md
@@ -8,11 +8,10 @@ bundle exec warble
 java -jar sorbet-jruby-example.jar
 ```
 
-## JRuby
+### Environment
+## JRuby version
 
 ```
 ruby --version
 jruby 9.1.17.0 (2.3.3) 2018-04-20 d8b1ff9 OpenJDK 64-Bit Server VM 25.232-b09 on 1.8.0_232-b09 +jit [darwin-x86_64]
-
-ruby bin/example.rb
 ```
--- bin/example.rb
+++ bin/example.rb
@@ -1,30 +1,4 @@
-# typed: true
-
 require 'rubygems'
-require 'sorbet-runtime'
+require 'demo'
 
-module Test
-  class A
-    extend T::Sig
-
-    sig {params(a: T.untyped).void}
-    def initialize(a)
-      @a = a
-    end
-
-  end
-end
-
-
-def run
-  c = Class.new(Test::A)
-  a ||= c.new(self)
-end
-
-threads = []
-(1..50).each do |i|
-  threads << Thread.new { run }
-end
-
-threads.each {|thread| thread.join }
-
+Demo.hello_world
